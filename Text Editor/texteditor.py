f6ff7f9e5d8e6391a81a3a5b86daa87f21b2c428
Luca Koval <lucakoval@me.com>
2016-03-05 20:31:42 -0500

    i might be creating a new label each time
    yee

--- Text Editor/texteditor.py
+++ Text Editor/texteditor.py
@@ -4,6 +4,12 @@ root = Tk()
 
 def key(event):
     print "pressed", repr(event.char)
+    # a = Label(root, wraplength=100000, text=event.char)
+    # a.pack()
+    var = StringVar()
+    label = Label(root, wraplength=10000, textvariable=var)
+    var.set(event.char)
+    label.pack()
 
 def callback(event):
     frame.focus_set()
