27afa958b89ff391cdac5ebf43621a01a73b62ae
Luca Koval <lucakoval@me.com>
2016-03-05 21:37:50 -0500

    Delete works
    yee

--- Text Editor/texteditor.py
+++ Text Editor/texteditor.py
@@ -4,8 +4,11 @@ root = Tk()
 
 def key(event):
     print "pressed", repr(event.char)
-    var.set(var.get() + event.char)
-    label.pack()
+    if (event.char == "\x7f"):
+    	var.set(var.get()[:-1])
+    else:
+	    var.set(var.get() + event.char)
+	    label.pack()
 
 def callback(event):
     frame.focus_set()
@@ -13,10 +16,14 @@ def callback(event):
 
 frame = Frame(root, width=100, height=100)
 var = StringVar()
-label = Label(root, justify=LEFT, padx=0, pady=0, textvariable=var)
+label = Label(root, anchor=W, justify=LEFT, padx=0, pady=0, textvariable=var)
 label.pack
 frame.bind("<Key>", key)
 frame.bind("<Button-1>", callback)
 frame.pack()
 
 root.mainloop()
+
+if not self.e.get().isdigit():
+     #get the length of the string in the widget, and subtract one, and delete everything up to the end
+     self.e.delete(len(self.e.get)-1, tk.END)
